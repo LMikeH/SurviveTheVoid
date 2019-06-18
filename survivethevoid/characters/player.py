87717a6ac0a2da461556f6a3474b81063116b90b
LMikeH <lmichaelhuang3@gmail.com>
2019-06-17 20:23:44 -0400

    Finished full screen, gj Alvin

--- survivethevoid/characters/player.py
+++ survivethevoid/characters/player.py
@@ -26,7 +26,7 @@ class Player(pygame.sprite.Sprite):
         super(Player, self).__init__()
         # Speed is vector with dx/dt, dy/dt, and d-angle/dt
         self.v = [0, 0]
-        self.dang = 0
+        self.omega = 0
         self.angle = angle
         self.screen = screen
         self.x = x
@@ -81,9 +81,9 @@ class Player(pygame.sprite.Sprite):
 
         # Angles
         if key_state[pygame.K_a]:
-            self.dang -= .01
+            self.omega -= .01
         elif key_state[pygame.K_d]:
-            self.dang += .01
+            self.omega += .01
 
     def update(self, key_state):
         """
@@ -99,7 +99,7 @@ class Player(pygame.sprite.Sprite):
 
         """
         self.controls(key_state)
-        self.rotate(self.dang)
+        self.rotate(self.omega)
 
         # True floating point positions.
         self.x += self.v[0]
