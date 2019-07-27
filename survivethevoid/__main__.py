0c0a164ff3e5fafe0cf4b9a00606f7914ebf6509
lealex262 <lealex262@gmail.com>
2019-07-27 15:03:07 -0400

    more core changes

--- survivethevoid/__main__.py
+++ survivethevoid/__main__.py
@@ -4,28 +4,9 @@ from survivethevoid.environment.asteroids import Asteroid
 from survivethevoid.core.game import Game
 
 def main():
-    """
-    This is the main loop.
-
-    """
-    pygame.init()
-    screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-    done = False
-    game = Game(screen)
-    background = pygame.Surface(screen.get_size())
-    background.fill((0, 0, 0))
-    pygame.display.update()
-
-    while not done:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                done = True
-        screen.blit(background, (0, 0))
-
-        game.update(pygame.key.get_pressed())
-        game.draw()
-
-        pygame.display.flip()
+    game = Game()
+    game.start()
+    game.run()
 
 
 if __name__ == "__main__":
--- survivethevoid/core/game.py
+++ survivethevoid/core/game.py
@@ -3,9 +3,12 @@ from survivethevoid.characters.player import Player
 from survivethevoid.environment.asteroids import Asteroid
 # from survivethevoid.environment.Camera import Camera
 
+# Setup
+corerandom = random.Random()
+
 
 class Game(object):
-    def __init__(self, screen):
+    def __init__(self):
         """
         This is the overall game class which controls events, players, and non-players.
 
@@ -15,7 +18,36 @@ class Game(object):
         passes in the display so sprites can be drawn.
 
         """
-        self.screen = screen
+        # Setup Variables
+        self.time = time.time()
+        self.clock = 0
+		self.ticks = 0
+		self.camera = [0, 0]
+
+        # Setup PyGame
+        pygame.init()
+        self.worlddimensions =
+        self.world =
+
+        # Setup Screen
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.world_background = pygame.Surface(worlddimensions)
+        self.world_background = background.convert()
+		self.world_background.fill((0, 0, 0))
+
+        # Debug surface
+		debug = pygame.Surface(worlddimensions)
+		debug = debug.convert()
+		debug.fill((255, 255, 255))
+		background.blit(debug, (0, 0))
+		screen.blit(background, (0, 0))
+		pygame.display.flip()
+
+    def start(self):
+        """
+        Create
+        """
+        # Setup Objects
         self.characters = pygame.sprite.Group()
         self.projectiles = pygame.sprite.Group()
         self.asteroids = pygame.sprite.Group()
@@ -23,7 +55,21 @@ class Game(object):
         self.one_asteroid = Asteroid(screen, 0, 0, [.1,.1,.1], 50)
         self.asteroids.add(self.one_asteroid)
         self.characters.add(self.hero)
-        # self.camera = Camera(self.screen.width, self.screen.height)
+
+    def run(self):
+        """
+        This is the game loop
+
+        """
+        # Time
+        clock = pygame.time.Clock()
+
+        # Game Loop
+        done = False
+        while not done:
+            self.handle_events()
+            self.update(pygame.key.get_pressed())
+            self.draw()
 
     def update(self, keys):
         """
@@ -57,3 +103,8 @@ class Game(object):
 
         if col != []:
             self.hero.kill()
+
+    def handle_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
