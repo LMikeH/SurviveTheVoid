4ca19169aeddb1b9ffac2c0a00f3f0a6386cfade
lealex262 <lealex262@gmail.com>
2019-08-04 16:46:16 -0400

    Added simple Health Bar

--- survivethevoid/core/game.py
+++ survivethevoid/core/game.py
@@ -118,16 +118,26 @@ class Game(object):
         -------
 
         """
+        # Display Player Information
         myfont = pygame.font.SysFont('monospace', 15)
         label1 = myfont.render("Coordinates: {} {} ".format('%.2f' % self.player.x, '%.2f' % self.player.y), 1, (255, 0, 0))
         label2 = myfont.render("Velocity:    {} {}".format('%.2f' % self.player.v[0], '%.2f' % self.player.v[1]), 1, (255, 0, 0))
+
+        # Display Game Information
         self.clock.tick(100)
         label3 = myfont.render("FPS: " + str(self.clock.get_fps()),  1, (255, 0, 0))
+
+        # Draw Screen
         self.screen.blit(self.background, (0, 0))
         self.camera_group.draw(self.screen)
         self.screen.blit(label1, (50, 50))
         self.screen.blit(label2, (50, 65))
         self.screen.blit(label3, (50, 75))
+
+        # Display Player Stats
+        pygame.draw.rect(self.screen, (255, 0, 0), (350, 50, 200, 30))
+        pygame.draw.rect(self.screen, (128, 255, 128), (350, 50, self.player.health * 2 , 30))
+
 
     def collision_check(self):
         col = pygame.sprite.groupcollide(self.characters, self.asteroids, False, False, pygame.sprite.collide_mask)
